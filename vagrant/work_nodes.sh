6ee61c575ef144070549a969a05e318ec0e64b1e
Blue Moon <harveymoons@gmail.com>
2022-07-23 20:40:06 +0900

    Update work_nodes.sh

--- vagrant/work_nodes.sh
+++ vagrant/work_nodes.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# pre set-up
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
+
 # config for work_nodes only 
 kubeadm join --token 123456.1234567890123456 \
              --discovery-token-unsafe-skip-ca-verification 192.168.1.10:6443
